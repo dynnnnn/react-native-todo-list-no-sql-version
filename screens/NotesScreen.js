ff000faad776916ae349b415a3bd689316166c77
gladyn <gladynwong@gmail.com>
2022-06-15 10:46:00 +0800

    delete task

--- screens/NotesScreen.js
+++ screens/NotesScreen.js
@@ -7,6 +7,9 @@ import {
   FlatList,
 } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
+import firebase from "../database/firebaseDB";
+
+const db = firebase.firestore().collection("todos");
 
 export default function NotesScreen({ navigation, route }) {
   const [notes, setNotes] = useState([]);
@@ -36,12 +39,33 @@ export default function NotesScreen({ navigation, route }) {
       const newNote = {
         title: route.params.text,
         done: false,
-        id: notes.length.toString(),
+        created: firebase.firestore.FieldValue.serverTimestamp(),
+        
       };
-      setNotes([...notes, newNote]);
+      db.add(newNote);
     }
   }, [route.params?.text]);
 
+  useEffect(
+    () => {
+    const unsubscribe = db.orderBy("created").onSnapshot((collection) => {
+        const updatedNotes = collection.docs.map((doc) => {
+          const noteObject = {
+            ...doc.data(),
+            id: doc.id,
+          };
+        console.log(noteObject);
+        return noteObject;
+        
+      });
+      setNotes(updatedNotes);
+    });
+    return () => {
+      unsubscribe()
+    };
+  }, []);
+
+  
   function addNote() {
     navigation.navigate("Add Screen");
   }
@@ -50,7 +74,8 @@ export default function NotesScreen({ navigation, route }) {
   function deleteNote(id) {
     console.log("Deleting " + id);
     // To delete that item, we filter out the item we don't want
-    setNotes(notes.filter((item) => item.id !== id));
+    db.doc(id).delete();
+
   }
 
   // The function to render each row in our FlatList
@@ -94,4 +119,4 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
-});
+})
